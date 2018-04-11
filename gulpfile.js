dc695f5898c22ec3b14008d8ac203967ff78e840
burrich <jezequel.n@gmail.com>
2018-04-11 22:43:55 +0200

    Update gulpfile : build dist, postcss...

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,116 @@
-const gulp         = require('gulp');
-const sass         = require('gulp-sass');
-const babel        = require('gulp-babel');
-const rename       = require('gulp-rename');
-const uglify       = require('gulp-uglify');
-const cleanCSS     = require('gulp-clean-css');
-const sourcemaps   = require('gulp-sourcemaps');
-const runSequence  = require('run-sequence');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp       = require('gulp');
+const sass       = require('gulp-sass');
+const babel      = require('gulp-babel');
+const rename     = require('gulp-rename');
+const uglify     = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
 
-const browserSync  = require('browser-sync').create();
+const runSequence = require('run-sequence');
+
+//  PostCSS
+const postcss      = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const uncss        = require('postcss-uncss');
+const cssnano      = require('cssnano');
+
+const browserSync = require('browser-sync').create();
+
+
+const browserSyncPort = 3100;
+
+const paths = {
+	dev: './',
+	prod: 'dist_toRemove',
+	src: 'src',
+	maps: 'maps'
+};
+
+// TODO: use rename() object argument => .pipe(rename({ extname: '.js' })
+// TODO: environment variable
+// TOD0: change tasks names (dist to suffix ?) => build:css
+
+gulp.task('default', ['serve']);
 
 gulp.task('serve', ['css', 'js'], function() {
 	browserSync.init({
 		server: {
 			baseDir: './'
 		},
-		port: 3100
+		port: browserSyncPort
 	});
 
-	gulp.watch('src/sass/**/*.scss', ['css']).on('change', browserSync.reload);
-	gulp.watch('src/main.js', ['js']).on('change', browserSync.reload);
+	gulp.watch(paths.src + '/sass/**/*.scss', ['css']).on('change', browserSync.reload);
+	gulp.watch(paths.src + '/main.js', ['js']).on('change', browserSync.reload);
 	gulp.watch('index.html').on('change', browserSync.reload);
 });
 
-// TODO: use rename() object argument => .pipe(rename({ extname: '.js' })
+// CSS
 
-// TODO: browsersync refresh only on dev AND not on serve
 gulp.task('css', function() {
 	return gulp.src('src/sass/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(rename('style.css'))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./'));
-		// .pipe(browserSync.stream());
+		.pipe(postcss([
+			autoprefixer()
+		]))
+		.pipe(sourcemaps.write(paths.maps))
+		.pipe(gulp.dest(paths.dev));
 });
+
+gulp.task('dist:css', function() {
+	return gulp.src('src/sass/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename('style.css'))
+		.pipe(postcss([
+			autoprefixer(),
+			uncss({ html: ['index.html'] }),
+			cssnano({ preset: 'default' })
+		]))
+		.pipe(sourcemaps.write(paths.maps))
+		.pipe(gulp.dest(paths.prod));
+});
+
+// JS
 
 gulp.task('js', function() {
 	return gulp.src('src/main.js')
 		.pipe(sourcemaps.init())
 		.pipe(babel())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./'));
+		.pipe(sourcemaps.write(paths.maps))
+		.pipe(gulp.dest(paths.dev));
 });
 
-// TODO: external sourcemaps
-gulp.task('min:css', function() {
-	return gulp.src('style.css')
-		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(rename('style.min.css'))
-		.pipe(gulp.dest('./'));
-
-		// .pipe(sourcemaps.init())
-		// .pipe(cleanCSS({compatibility: 'ie8'}))
-		// .pipe(sourcemaps.write())
-});
-
-// TODO: pump (error handling)
-gulp.task('min:js', function() {
-	return gulp.src('main.js')
+// TODO: pump (error handling) => see uglify doc
+gulp.task('dist:js', function() {
+	return gulp.src('src/main.js')
+	.pipe(sourcemaps.init())
+	.pipe(babel())
 	.pipe(uglify())
-	.pipe(rename('main.min.js'))
-	.pipe(gulp.dest('./'));
+	.pipe(sourcemaps.write(paths.maps))
+	.pipe(gulp.dest(paths.prod));
 });
 
-gulp.task('copy', ['copy:css', 'copy:js', 'copy:img'], function() {
+// Copy
+
+gulp.task('copy', ['copy:img'], function() {
 	return gulp.src(['index.html', 'cv.pdf'])
-		.pipe(gulp.dest('dist_toRemove'));
-});
-
-// TODO: copy: refacto (DRY)
-// TODO: remove min files ?
-
-gulp.task('copy:css', function() {
-	return gulp.src('style.min.css')
-		.pipe(rename('style.css'))
-		.pipe(gulp.dest('dist_toRemove'));
-});
-
-gulp.task('copy:js', function() {
-	return gulp.src('main.min.js')
-		.pipe(rename('main.js'))
-		.pipe(gulp.dest('dist_toRemove'));
+		.pipe(gulp.dest(paths.prod));
 });
 
 gulp.task('copy:img', function() {
 	return gulp.src('img/*')
-		.pipe(gulp.dest('dist_toRemove/img'));
+		.pipe(gulp.dest(paths.prod + '/img'));
 });
 
-// TODO: min css depend on variable env ?
-gulp.task('deploy', function() {
+// Build
+
+gulp.task('build', ['css', 'js']);
+
+gulp.task('dist:build', function() {
 	runSequence(
-		['css', 'js'],
-		['min:css', 'min:js'],
+		['dist:css', 'dist:js'],
 		'copy'
 	);
 });
-
-gulp.task('default', ['serve']);
